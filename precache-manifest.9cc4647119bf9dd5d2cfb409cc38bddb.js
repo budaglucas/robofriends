a78b1588138acd75b49393a8bfe393d02468c8eb
Lucas <budaglucas@gmail.com>
2020-05-18 20:49:54 -0300

    Updates

---
+++ precache-manifest.9cc4647119bf9dd5d2cfb409cc38bddb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bbf124fa6fe4ce15dd55ca57d6a2448",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "cbcb14e194d6425e5a44",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e9f3526ad013e4488e6c",
+    "url": "/robofriends/static/css/main.a9cbc974.chunk.css"
+  },
+  {
+    "revision": "cbcb14e194d6425e5a44",
+    "url": "/robofriends/static/js/2.800020c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.800020c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9f3526ad013e4488e6c",
+    "url": "/robofriends/static/js/main.6f80cdc5.chunk.js"
+  },
+  {
+    "revision": "4b54cd1813776aa306f7",
+    "url": "/robofriends/static/js/runtime-main.de469359.js"
+  }
+]);
